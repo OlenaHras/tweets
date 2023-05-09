1dd26632309d786b75b6b51c4844d0b8490ee5c5
OlenaHras <alena.hras@gmail.com>
2023-05-09 16:19:23 +0300

    added basename to router

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter
-    // basename="/tweets"
-    >
+    <BrowserRouter basename="/tweets">
       <App />
     </BrowserRouter>
   </Provider>
