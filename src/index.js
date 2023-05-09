ea284f4ed55f99eb2a65b27983d29e465880ddea
OlenaHras <alena.hras@gmail.com>
2023-05-09 18:46:20 +0300

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/tweets">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </Provider>
